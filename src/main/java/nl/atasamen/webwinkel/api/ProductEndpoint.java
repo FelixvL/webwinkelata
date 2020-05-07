cba7adcc3dc4f2825bce3877576de369320b92f7
FelixvL <digitaleservice@gmail.com>
2020-05-07 13:59:08 +0200

    Eerste commit properties eruit

---
+++ src/main/java/nl/atasamen/webwinkel/api/ProductEndpoint.java
@@ -0,0 +1,13 @@
+package nl.atasamen.webwinkel.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductEndpoint {
+
+	@GetMapping("/allproducts")
+	public String getAllProducts() {
+		return "Mijn Lijst met Producten";
+	}
+}
