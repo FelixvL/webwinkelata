7f48cef80878310e687d3575a89062c612087ace
FelixvL <digitaleservice@gmail.com>
2020-05-07 16:28:09 +0200

    get en post

--- src/main/java/nl/atasamen/webwinkel/api/ProductEndpoint.java
+++ src/main/java/nl/atasamen/webwinkel/api/ProductEndpoint.java
@@ -1,13 +1,30 @@
 package nl.atasamen.webwinkel.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import nl.atasamen.webwinkel.controller.ProductService;
+import nl.atasamen.webwinkel.domein.Product;
 
 @RestController
 public class ProductEndpoint {
+	
+	@Autowired
+	ProductService ps;
 
 	@GetMapping("/allproducts")
-	public String getAllProducts() {
-		return "Mijn Lijst met Producten!";
+	public Iterable<Product> getAllProducts() {
+		Iterable<Product> producten =  ps.getAllProducts();
+		return producten;
+	}
+	@PostMapping("/newproduct")
+	public void maakNieuwProduct(@RequestBody Product product) {
+		System.out.println("we zijn in nieuw product "+product.getNaam());
+		ps.slaProductOp(product);
+		
+		
 	}
 }
---
+++ src/main/java/nl/atasamen/webwinkel/domein/Product.java
@@ -0,0 +1,46 @@
+package nl.atasamen.webwinkel.domein;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String naam;
+	private String omschrijving;
+	private int prijs;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public String getOmschrijving() {
+		return omschrijving;
+	}
+	public void setOmschrijving(String omschrijving) {
+		this.omschrijving = omschrijving;
+	}
+	public int getPrijs() {
+		return prijs;
+	}
+	public void setPrijs(int prijs) {
+		this.prijs = prijs;
+	}
+	
+
+	
+}
